371cab462b24231c9546c69a0d359a477e50ba6e
Léo Viggiano <leoviggiano1@outlook.com>
2019-09-07 01:39:35 -0300

    add: Main Destination slider

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,16 +1,120 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+import {
+  Container,
+  ContainerColumn,
+  ContainerRating,
+  ContainerHeader,
+  SearchText,
+  Title,
+  Avatar,
+  Articles,
+  RenderContainer,
+  DestinationsText,
+  RecommendedText,
+  Scroll,
+  DestinationView,
+  UserImage,
+  UserName,
+  UserLocation,
+  ItemRating,
+  DestinationItemView,
+  DestinationInfo,
+  DBackground,
+  DInfoTitle,
+  DInfoDesc,
+  RecommendedView,
+} from './styles';
+
+import { mocks } from './mocks';
 
 export default class List extends React.Component {
+  static navigationOptions = {
+    header: (
+      <ContainerHeader>
+        <Container>
+          <SearchText>Search for place</SearchText>
+          <Title>Destination</Title>
+        </Container>
+        <Avatar
+          source={{ uri: 'https://randomuser.me/api/portraits/women/57.jpg' }}
+        />
+      </ContainerHeader>
+    ),
+  };
+
+  renderDestinations = () => {
+    const { destinations } = this.props;
+    return (
+      <RenderContainer>
+        <Scroll
+          data={destinations}
+          style={{ overflow: 'visible' }}
+          keyExtractor={(item, index) => `${item.id}`}
+          renderItem={({ item }) => this.renderDestination(item)}
+        >
+          <DestinationView>
+            <DestinationsText>Destination 1</DestinationsText>
+          </DestinationView>
+
+          <DestinationView>
+            <DestinationsText>Destination 2</DestinationsText>
+          </DestinationView>
+
+          <DestinationView>
+            <DestinationsText>Destination 3</DestinationsText>
+          </DestinationView>
+        </Scroll>
+      </RenderContainer>
+    );
+  };
+
+  renderDestination = item => {
+    return (
+      <DBackground source={{ uri: item.preview }}>
+        <DestinationItemView>
+          <Container>
+            <UserImage source={{ uri: item.user.avatar }} />
+          </Container>
+
+          <ContainerColumn>
+            <UserName>{item.user.name}</UserName>
+            <UserLocation>{item.location}</UserLocation>
+          </ContainerColumn>
+
+          <ContainerRating>
+            <ItemRating>{item.rating}</ItemRating>
+          </ContainerRating>
+        </DestinationItemView>
+        <DestinationInfo>
+          <DInfoTitle>{item.title}</DInfoTitle>
+          <DInfoDesc>{item.description}</DInfoDesc>
+        </DestinationInfo>
+      </DBackground>
+    );
+  };
+
+  renderRecommended = () => {
+    return (
+      <RenderContainer>
+        <RecommendedView>
+          <RecommendedText>Recommended</RecommendedText>
+        </RecommendedView>
+      </RenderContainer>
+    );
+  };
+
   render() {
     return (
-      <View>
-        <Text>List</Text>
-      </View>
+      <Articles>
+        {this.renderDestinations()}
+        {this.renderRecommended()}
+      </Articles>
     );
   }
 }
 
-List.navigationOptions = {
-  title: 'List',
+List.defaultProps = {
+  destinations: mocks,
 };
--- src/screens/List/styles.js
+++ src/screens/List/styles.js
@@ -1,6 +1,128 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 
-export const Container = styled.View`
+const { width, height } = Dimensions.get('screen');
+
+// Default
+export const Container = styled.View``;
+export const ContainerColumn = styled.View`
   flex: 1;
-  margin-top: 24px;
+  flex-direction: column;
 `;
+
+// Header
+export const ContainerHeader = styled.View`
+  flex: 1;
+  flex-direction: row;
+  background-color: white;
+  padding: 48px 36px;
+  padding-bottom: 24px;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const SearchText = styled.Text``;
+
+export const Avatar = styled.Image`
+  width: 36px;
+  height: 36px;
+  border-radius: 18px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+`;
+
+// Main Container
+export const Articles = styled.View`
+  flex: 1;
+`;
+
+export const RenderContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+`;
+
+export const Scroll = styled.FlatList.attrs({
+  horizontal: true,
+  pagingEnabled: true,
+  scrollEnabled: true,
+  showsHorizontalScrollIndicator: false,
+  scrollEventThrottle: 16,
+  snapToAlignment: 'center',
+})`
+  overflow: visible;
+`;
+
+// Destination
+export const DestinationView = styled.View`
+  width: ${width};
+  border-radius: 12px;
+  padding: 36px;
+  margin: 0 36px;
+`;
+export const DestinationItemView = styled.View`
+  flex: 1;
+  flex-direction: row;
+`;
+export const DestinationsText = styled.Text``;
+export const DBackground = styled.ImageBackground.attrs({
+  imageStyle: { borderRadius: 12 },
+})`
+  flex: 1;
+  width: ${width - 36 * 2};
+  margin: 0 36px;
+  padding: 36px;
+`;
+
+export const UserImage = styled.Image`
+  margin-right: 20px;
+  width: 36px;
+  height: 36px;
+  border-radius: 18px;
+`;
+
+export const UserName = styled.Text`
+  font-weight: bold;
+  color: white;
+`;
+export const UserLocation = styled.Text`
+  color: white;
+`;
+export const ItemRating = styled.Text`
+  font-size: 28px;
+  font-weight: bold;
+  color: white;
+`;
+
+export const ContainerRating = styled.View`
+  align-items: flex-end;
+`;
+
+export const DestinationInfo = styled.View`
+  position: absolute;
+  border-radius: 12px;
+  padding: 18px 36px;
+  bottom: -36px;
+  right: 36px;
+  left: 36px;
+  box-shadow: 0px 1px 4px black;
+  background-color: white;
+  z-index: 3;
+  overflow: visible;
+`;
+
+export const DInfoTitle = styled.Text`
+  font-weight: 500;
+  font-size: 18px;
+  padding: 8px 0;
+`;
+
+export const DInfoDesc = styled.Text`
+  color: #667;
+`;
+
+// Recommended
+export const RecommendedView = styled.View`
+  padding: 36px;
+`;
+export const RecommendedText = styled.Text``;
